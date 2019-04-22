75bf450f2c11376a4278f06e605dd4d6b74aa9a7
yangroro <yang@heechan.kr>
2019-04-22 17:55:03 +0900

    Add GitLab runner docker compose files

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose.config.yml up
+docker-compose -f docker-compose.yml -f docker-compose.register.yml up
+docker-compose up -d
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://get.docker.com/ | sudo sh
+sudo usermod -aG docker $USER
